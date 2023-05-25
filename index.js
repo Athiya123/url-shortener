00a7833e97f8a784d143f90b1bb79b35f7f1ec9b
Thadwai Greeshmika <thadwaigreeshmika5@gmail.com>
2023-05-25 16:17:31 +0530

    step 4

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
     return res.send("welcome");
 })
+
 app.get('/i', (req, res) => {
     const endpoints = [
       { path: '/i/google', description: 'Google website' },
@@ -17,6 +19,7 @@ app.get('/i', (req, res) => {
   
     res.send(`<ul>${response}</ul>`);
   });
+
 app.get('/i/github', (req, res) => {
     res.send('<a href="https://github.com">GitHub</a>');
   });
@@ -28,14 +31,31 @@ app.get('/i/google',(req,res)=>{
 app.get('/r/google',(req,res)=>{
     res.redirect('https://google.com');
 })
+
 app.get('/r/github',(req,res)=>{
     res.redirect('https://github.com');
 })
 
 app.get('/r',(req,res)=>{
-     const msg = `<b>To redirect the website for Google and Github use the endpoints as: <br><br> <li>/r/google to visit the google website</li> <br> <li>/r/github to visit the github website</li>`;
+    const msg = `<b>To redirect the website for Google and Github use the endpoints as: <br><br> <li>/r/google to visit the google website</li> <br> <li>/r/github to visit the github website</li>`;
     res.send(msg);
 })
+
+const mappings = {};
+
+app.post('/map', (req, res) => {
+  const { alias, url } = req.body;
+  if (!alias || !url) {
+    return res.status(400).json({ error: 'Both alias and URL must be provided.' });
+  }
+  mappings[alias] = url;
+  res.status(201).json({ message: 'Mapping is created successfully.' });
+});
+
+app.get('/mappings', (req, res) => {
+  res.json(mappings);
+});
+
 app.listen(3002,()=>{
     console.log("welcome on port 3002");
 })
