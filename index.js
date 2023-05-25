b32abe564f0e73b252add9c2455a9690a8df051d
Thadwai Greeshmika <thadwaigreeshmika5@gmail.com>
2023-05-25 11:24:14 +0530

    step 2

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ const app = express();
 app.get('/',(req,res)=>{
     return res.send("welcome");
 })
+app.get('/i', (req, res) => {
+    const endpoints = [
+      { path: '/i/google', description: 'Google website' },
+      { path: '/i/github', description: 'GitHub website' }
+    ];
+    const response = endpoints
+      .map(endpoint => `<li>${endpoint.path}: ${endpoint.description}</li>`)
+      .join('');
+  
+    res.send(`<ul>${response}</ul>`);
+  });
+app.get('/i/github', (req, res) => {
+    res.send('<a href="https://github.com">GitHub</a>');
+  });
+  
+app.get('/i/google',(req,res)=>{
+    res.send('<a href="https://www.google.com">Google</a>')
+})
 app.listen(3002,()=>{
     console.log("welcome on port 3002");
 })
