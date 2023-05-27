5efbf79e804b19e42e9d183e8af6e2f97f17a02a
Thadwai Greeshmika <thadwaigreeshmika5@gmail.com>
2023-05-27 17:40:30 +0530

    step 6

--- index.js
+++ index.js
@@ -1,73 +1,112 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const { connectToDatabase } = require('./db');
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=>{
-    return res.send("welcome");
+mongoose.connect('mongodb+srv://greesh_5:munny123@cluster0.lvfzzc5.mongodb.net/URL', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
 
-app.get('/i', (req, res) => {
-    const endpoints = [
-      { path: '/i/google', description: 'Google website' },
-      { path: '/i/github', description: 'GitHub website' }
-    ];
-    const response = endpoints
-      .map(endpoint => `<li>${endpoint.path}: ${endpoint.description}</li>`)
-      .join('');
+    const mappingSchema = new mongoose.Schema({
+      alias: String,
+      url: String,
+    });
+
+    const Mapping = mongoose.model('Mapping', mappingSchema);
+
+    app.post('/map', async (req, res) => {
+      const { alias, url } = req.body;
+      try {
+        const newMapping = new Mapping({ alias, url });
+        await newMapping.save();
+        res.status(201).json({ message: 'Mapping saved successfully' });
+      } catch (error) {
+        console.error('Error saving mapping:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
+    app.get('/', (req, res) => {
+      return res.send("welcome");
+    })
+
+    app.get('/i', (req, res) => {
+      const endpoints = [
+        { path: '/i/google', description: 'Google website' },
+        { path: '/i/github', description: 'GitHub website' }
+      ];
+      const response = endpoints
+        .map(endpoint => `<li>${endpoint.path}: ${endpoint.description}</li>`)
+        .join('');
+
+      res.send(`<ul>${response}</ul>`);
+    });
+
+    app.get('/i/github', (req, res) => {
+      res.send('<a href="https://github.com">GitHub</a>');
+    });
+
+    app.get('/i/google', (req, res) => {
+      res.send('<a href="https://www.google.com">Google</a>')
+    })
+
+    app.get('/r/google', (req, res) => {
+      res.redirect('https://google.com');
+    })
+
+    app.get('/r/github', (req, res) => {
+      res.redirect('https://github.com');
+    })
+
+    app.get('/r', (req, res) => {
+      const msg = `<b>To redirect the website for Google and Github use the endpoints as: <br><br> <li>/r/google to visit the google website</li> <br> <li>/r/github to visit the github website</li>`;
+      res.send(msg);
+    })
+
+    const mappings = {};
+
+    app.post('/map', async (req, res) => {
+      const { alias, url } = req.body;
+      const db = getDatabase();
+      connectToDatabase();
+      const collection = db.collection('URL');
+      try {
+        await collection.insertOne({ alias, url });
+        res.status(201).json({ message: 'Mapping saved successfully' });
+      } catch (error) {
+        console.error('Error saving mapping:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
   
-    res.send(`<ul>${response}</ul>`);
+    app.get('/mappings', async (req, res) => {
+      try {
+        const mappings = await Mapping.find();
+        res.json(mappings);
+      } catch (error) {
+        console.error('Error retrieving mappings:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
+    app.get('/r/:alias', (req, res) => {
+      const { alias } = req.params;
+
+      if (mappings.hasOwnProperty(alias)) {
+        const url = mappings[alias];
+        res.redirect(url);
+      } else {
+        res.sendStatus(404);
+      }
+    });
+    app.listen(3002, () => {
+      console.log("welcome on port 3002");
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
   });
-
-app.get('/i/github', (req, res) => {
-    res.send('<a href="https://github.com">GitHub</a>');
-  });
-  
-app.get('/i/google',(req,res)=>{
-    res.send('<a href="https://www.google.com">Google</a>')
-})
-
-app.get('/r/google',(req,res)=>{
-    res.redirect('https://google.com');
-})
-
-app.get('/r/github',(req,res)=>{
-    res.redirect('https://github.com');
-})
-
-app.get('/r',(req,res)=>{
-    const msg = `<b>To redirect the website for Google and Github use the endpoints as: <br><br> <li>/r/google to visit the google website</li> <br> <li>/r/github to visit the github website</li>`;
-    res.send(msg);
-})
-
-const mappings = {};
-
-app.post('/map', (req, res) => {
-  const { alias, url } = req.body;
-  if (!alias || !url) {
-    return res.status(400).json({ error: 'Both alias and URL must be provided.' });
-  }
-  mappings[alias] = url;
-  res.status(201).json({ message: 'Mapping is created successfully.' });
-});
-
-app.get('/mappings', (req, res) => {
-  res.send(mappings);
-});
-
-app.get('/r/:alias', (req, res) => {
-  const { alias } = req.params;
-
-  if (mappings.hasOwnProperty(alias)) {
-    const url = mappings[alias];
-    res.redirect(url);
-  } else {
-    res.sendStatus(404);
-  }
-});
-
-
-app.listen(3002,()=>{
-    console.log("welcome on port 3002");
-})
