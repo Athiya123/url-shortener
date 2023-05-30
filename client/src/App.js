51c6e87a0065469fc6907fb814cdb7744e0d5fe4
Athiya Fathima <85676437+Athiya123@users.noreply.github.com>
2023-05-30 19:28:23 +0530

    Done with styling in fronted part

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import './App.css';
+
 function App() {
   const [url, setUrl] = useState('');
   const [shortenedUrl, setShortenedUrl] = useState('');
   const [isValidUrl, setIsValidUrl] = useState(true);
+
   useEffect(() => {
-    // Regular expression pattern for URL validation
+    // usin regex for validation
     const urlPattern = /^(?:(?:(?:https?|ftp):)?\/\/)(?!(?:.*ww\.)){1}(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
     const numericPattern = /^\d+$/;
+    
     // Check if the entered URL matches the pattern, but only if the URL has been modified by the user
     if (url.trim() !== '') {
       setIsValidUrl(urlPattern.test(url) || numericPattern.test(url));
     }
   }, [url]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!isValidUrl) {
@@ -33,8 +38,10 @@ function App() {
       console.error('Error:', error);
     }
   };
+
   return (
     <div className="App">
+     <div className="card">
       <h1>URL Shortener</h1>
       <form onSubmit={handleSubmit}>
         <input
@@ -55,7 +62,9 @@ function App() {
           </a>
         </div>
       )}
+      </div>
     </div>
   );
 }
+
 export default App;
